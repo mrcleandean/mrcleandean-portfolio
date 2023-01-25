35acb613d6bda50a6f0b9bd3f20fcfe452fc8efa
Dean Kadri <noahshavekadri@Noahs-MacBook-Pro.local>
2023-01-24 22:44:07 -0700

    global update

--- client/src/components/BigLetter.js
+++ client/src/components/BigLetter.js
@@ -1,6 +1,8 @@
+import { useEffect, useRef, useState } from "react"
+
 export default function BigLetter(props) {
     return (
-        <span className={`text-5xl ${props.l === ' ' ? 'block w-3' : 'inline'} hover:text-blue-300 hover:text-6xl transition-all`}>
+        <span className={`text-5xl ${props.l === ' ' ? 'block w-3' : 'inline'} text-white hover:text-blue-300 hover:text-6xl transition-all`}>
             {props.l}
         </span>
     )
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import BigLetter from "../components/BigLetter";
+import anime from "animejs";
 const bigT = ['Clean Dean'];
 export default function Portfolio() {
   return (
@@ -8,11 +8,14 @@ export default function Portfolio() {
       `}>
         <div>
           <div>
-            <h1 className="text-white font-bigT flex items-center h-10">
-              {bigT.map((letter, i) => <BigLetter key={i} l={letter} />)}
-            </h1>
+            <h1 className="text-5xl text-white font-bigT flex items-center h-10">Clean Dean</h1>
           </div>
           <p className="mt-3 ml-2 text-indigo-400 font-mono">Web Developer</p>
+        </div>
+      </div>
+      <div className=" bg-blue-400 w-screen h-screen max-h-96">
+        <div>
+
         </div>
       </div>
     </div>
